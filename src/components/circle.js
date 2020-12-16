8fe67fe2a4b0bc5742c8cdad8da2261a96c0404b
vardhan2 <68744020+vardhan2@users.noreply.github.com>
2020-12-16 19:40:18 +0530

    updation

--- src/components/circle.js
+++ src/components/circle.js
@@ -2,5 +2,18 @@
 
 // declare class
 
+// export class using module.exports
+
+const shape = require("./shape");
+
+// declare class
+class Circle extends shape {
+  constructor() {
+    super();
+    this.color = shape.color;
+  }
+  calculateArea() {}
+}
 
 // export class using module.exports
+module.exports = Circle;
--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,13 @@
 // declare class
 
 // export class using module.exports
+class shape {
+  constructor() {
+    this.color = "green";
+  }
+  drawShape() {}
+  calculateArea() {}
+}
+
+// export class using module.exports
+module.exports = shape;
